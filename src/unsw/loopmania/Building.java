741ad5751d6d5061a23c7d470111fbecbb1b1c5b
Cameron Khuu <cameronkhuu99p@gmail.com>
2021-07-13 22:13:20 +1000

    Fixed wording of function in building to make it more universal

--- src/unsw/loopmania/Building.java
+++ src/unsw/loopmania/Building.java
@@ -11,72 +11,72 @@ public class Building extends StaticEntity {
     }
 
     public PathPosition possiblyGetSpawnPosition(List<Pair<Integer, Integer>> path) {
-        // 8 path tiles possibly arround the vampire castle
-        Pair<Integer, Integer> castlePosLeft = new Pair<Integer, Integer>(this.getX() - 1, this.getY());
-        Pair<Integer, Integer> castlePosRight = new Pair<Integer, Integer>(this.getX() + 1, this.getY());
-        Pair<Integer, Integer> castlePosUp  = new Pair<Integer, Integer>(this.getX(), this.getY() + 1);
-        Pair<Integer, Integer> castlePosDown = new Pair<Integer, Integer>(this.getX(), this.getY() - 1);
-        Pair<Integer, Integer> castlePosLeftUp = new Pair<Integer, Integer>(this.getX() - 1, this.getY() + 1);
-        Pair<Integer, Integer> castlePosRightUp = new Pair<Integer, Integer>(this.getX() + 1, this.getY() + 1);
-        Pair<Integer, Integer> castlePosLeftDown = new Pair<Integer, Integer>(this.getX() - 1, this.getY() - 1);
-        Pair<Integer, Integer> castlePosRightDown = new Pair<Integer, Integer>(this.getX() + 1, this.getY() - 1);
+        // 8 path tiles possibly arround the building
+        Pair<Integer, Integer> PosLeft = new Pair<Integer, Integer>(this.getX() - 1, this.getY());
+        Pair<Integer, Integer> PosRight = new Pair<Integer, Integer>(this.getX() + 1, this.getY());
+        Pair<Integer, Integer> PosUp  = new Pair<Integer, Integer>(this.getX(), this.getY() + 1);
+        Pair<Integer, Integer> PosDown = new Pair<Integer, Integer>(this.getX(), this.getY() - 1);
+        Pair<Integer, Integer> PosLeftUp = new Pair<Integer, Integer>(this.getX() - 1, this.getY() + 1);
+        Pair<Integer, Integer> PosRightUp = new Pair<Integer, Integer>(this.getX() + 1, this.getY() + 1);
+        Pair<Integer, Integer> PosLeftDown = new Pair<Integer, Integer>(this.getX() - 1, this.getY() - 1);
+        Pair<Integer, Integer> PosRightDown = new Pair<Integer, Integer>(this.getX() + 1, this.getY() - 1);
 
-        boolean castlePosLeftBool = false;
-        boolean castlePosRightBool = false;
-        boolean castlePosUpBool = false;
-        boolean castlePosDownBool = false;
-        boolean castlePosLeftUpBool = false;
-        boolean castlePosRightUpBool = false;
-        boolean castlePosLeftDownBool = false;
-        boolean castlePosRightDownBool = false;
+        boolean PosLeftBool = false;
+        boolean PosRightBool = false;
+        boolean PosUpBool = false;
+        boolean PosDownBool = false;
+        boolean PosLeftUpBool = false;
+        boolean PosRightUpBool = false;
+        boolean PosLeftDownBool = false;
+        boolean PosRightDownBool = false;
 
-        if(path.contains(castlePosLeft)) {
-            castlePosLeftBool = true;
+        if(path.contains(PosLeft)) {
+            PosLeftBool = true;
         }
-        if(path.contains(castlePosRight)) {
-            castlePosRightBool = true;
+        if(path.contains(PosRight)) {
+            PosRightBool = true;
         }
-        if(path.contains(castlePosUp)) {
-            castlePosUpBool = true;
+        if(path.contains(PosUp)) {
+            PosUpBool = true;
         }
-        if(path.contains(castlePosDown)) {
-            castlePosDownBool = true;
+        if(path.contains(PosDown)) {
+            PosDownBool = true;
         }
-        if(path.contains(castlePosLeftUp)) {
-            castlePosLeftUpBool = true;
+        if(path.contains(PosLeftUp)) {
+            PosLeftUpBool = true;
         }
-        if(path.contains(castlePosRightUp)) {
-            castlePosRightUpBool = true;
+        if(path.contains(PosRightUp)) {
+            PosRightUpBool = true;
         }
-        if(path.contains(castlePosLeftDown)) {
-            castlePosLeftDownBool = true;
+        if(path.contains(PosLeftDown)) {
+            PosLeftDownBool = true;
         }
-        if(path.contains(castlePosRightDown)) {
-            castlePosRightDownBool = true;
+        if(path.contains(PosRightDown)) {
+            PosRightDownBool = true;
         }
         PathPosition spawnPos = null;
 
         // Spawns from order left path tile and then preceeds clockwise 
         // If there are no immediate squares left, up, right or down, it will 
-        // try spawn a zombie in the diagonals starting from left down and preceeds clockwise
-        if(castlePosLeftBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosLeft), path);
-        } else if(castlePosUpBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosUp), path);
-        } else if(castlePosRightBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosRight), path);
-        } else if(castlePosDownBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosDown), path);
-        } else if(castlePosLeftDownBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosLeftDown), path);
-        } else if(castlePosLeftUpBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosLeftUp), path);
-        } else if(castlePosRightUpBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosRightUp), path);
-        } else if(castlePosRightDownBool) {
-            spawnPos = new PathPosition(path.indexOf(castlePosRightDown), path);
+        // try spawn in the diagonals starting from left down and preceeds clockwise
+        if(PosLeftBool) {
+            spawnPos = new PathPosition(path.indexOf(PosLeft), path);
+        } else if(PosUpBool) {
+            spawnPos = new PathPosition(path.indexOf(PosUp), path);
+        } else if(PosRightBool) {
+            spawnPos = new PathPosition(path.indexOf(PosRight), path);
+        } else if(PosDownBool) {
+            spawnPos = new PathPosition(path.indexOf(PosDown), path);
+        } else if(PosLeftDownBool) {
+            spawnPos = new PathPosition(path.indexOf(PosLeftDown), path);
+        } else if(PosLeftUpBool) {
+            spawnPos = new PathPosition(path.indexOf(PosLeftUp), path);
+        } else if(PosRightUpBool) {
+            spawnPos = new PathPosition(path.indexOf(PosRightUp), path);
+        } else if(PosRightDownBool) {
+            spawnPos = new PathPosition(path.indexOf(PosRightDown), path);
         } else {
-            throw new IllegalArgumentException("Vampire castle is not adjacent to a Path tile\n");
+            throw new IllegalArgumentException("Building is not adjacent to a Path tile\n");
         }
         
         return spawnPos;
